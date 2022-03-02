0c6c27603cbcc121f1227e2d6a51590473d55caa
Bardisk <xmjjsw@qq.com>
2022-03-02 15:47:01 +0800

    addtst

---
+++ tester.cpp
@@ -0,0 +1,22 @@
+#include<conio.h>
+#include<cstdio>
+#include<windows.h>
+#include<ctime>
+#include<cstdlib>
+int main(){
+	puts("--TESTOR--\nPress any key...");
+	while(1){
+//		Sleep(900);
+		if(_kbhit()){
+			char ch=_getch();
+			if(ch==0x1b){
+				puts("The program is terminating...");
+				Sleep(500);
+				break;
+			}
+			system("cls");
+			puts("--TESTOR--\nPress any key...");
+			printf("u've pressed: %c \n",ch);
+		}
+	}
+}
