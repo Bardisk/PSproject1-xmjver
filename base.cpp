551dd2796146b9126826b0d3d018fc12aeda2d7e
Bardisk <xmjjsw@qq.com>
2022-03-07 16:06:05 +0800

    update the node-info

---
+++ base.cpp
@@ -0,0 +1,60 @@
+#include "base.h"
+
+#define B_AND_I_ERROR 0xfffb
+
+nodeInfo::nodeInfo(const unsigned int &typ){
+	type = typ & 65535;
+	if (type & IS_ITEM) {
+		info.value = type >> 16;
+		if (type & SPEED_UP) s2 = 'S';
+		if (type & LEVEL_UP) s2 = 'L';
+	}
+	else s2 = 'N';
+	
+	if (type & SOFT_WALL)
+		s1 = 'S';
+	if (type & HARD_WALL)
+		s1 = 'H';
+	if (type & FLOOR_BLOCK)
+		s1 = 'F';
+	if (type & BOMB) {
+		s1 = 'B';
+		if (s2 != 'N')
+			typ = B_AND_I_ERROR;
+		else {
+			info.bvalue.level = (type >> 16) & 255;
+			info.bvalue.lastTime = type >> 24;
+		}
+	}
+}
+
+nodeInfo node::getDesc(char *Desc){
+	nodeInfo now(type);
+	if(type & 0x8000) sprintf(Desc, "ERROR!\n");
+	sprintf(Desc, "************************\n");
+	sprintf(Desc, "NODE INFORMATION\n");
+	switch (s1) {
+		case 'H':
+			sprintf(Desc, "type: H(HARD_WALL)\n");
+			break;
+		case 'S':
+			sprintf(Desc, "type: S(SOFT_WALL)\n");
+			break;
+		case 'F':
+			sprintf(Desc, "type: F(FLOOR_BLOCK)\n")
+			break;
+		case 'B':
+			sprintf(Desc, "type: B(BOMB) with level:%d and last_time:%d\n", now.info.bvalue.level, now.info.bvalue.lastTime);
+			break;
+	}
+	switch (s2) {
+		case 'S':
+			sprintf(Desc, "item: S(SPEED_UP) with value:%d\n", now.info.value);
+			break;
+		case 'L':
+			sprintf(Desc, "item: L(LEVEL_UP) with value:%d\n", now.info.value);
+			break;
+	}
+	sprintf(Desc, "************************\n");
+	return now;
+}
--- base.h
+++ base.h
@@ -11,11 +11,38 @@ struct pair{
 	int x, y;
 	pair(int xx=0, int yy=0) : x(xx), y(yy) {};
 };
-struct node{
-	int type, valuex, valuey;
-	node(int t=0, int vx=0, int vy=0) : type(t), valuex(vx), valuey(vy) {};
+
+//type accounts for lower 16 bits.
+//for bomb higher 8 bits for last-time, lower 8 bits for level.
+//for others higher 16 bits accounts for value.
+#define FLOOR_BLOCK 0x00000001
+#define HARD_WALL 0x00000002
+#define SOFT_WALL 0x00000004
+#define BOMB 0x00000008
+#define HAS_ITEM 0x00000010
+#define SPEED_UP 0x00000020
+#define LEVEL_UP 0x00000040
+
+struct nodeInfo{
+	short type;
+	char s1,s2;
+	union{
+		struct{
+			short lastTime;
+			short level;
+		}bvalue;
+		int value;
+	}info;
+	nodeInfo(const unsigned int &typ);
 };
+//8Byte
 
-
+struct node{
+	unsigned int type;
+	node(const unsigned int &t=0) : type(t) {};
+	inline nodeInfo getInfo(){return nodeInfo(type);};
+	nodeInfo getDesc(char *Desc);
+};
+//4Byte
 
 #endif
